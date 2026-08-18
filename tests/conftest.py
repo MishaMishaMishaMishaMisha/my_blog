3253ccff179dde5996bd49cb0b542bd1bf9b06e9
BlackHunterok <misha162534@gmail.com>
2026-08-18 16:24:25 +0300

    fix test db

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,8 +6,3 @@ def check_env_file():
     
     from source.core.config import settings
     assert settings.MODE == "TEST"
-    
-    # from source.core.logger import default_logger
-    # import logging
-    # # логи ставим на INFO
-    # default_logger.setLevel(logging._nameToLevel["INFO"])
